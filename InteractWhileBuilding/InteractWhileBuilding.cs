f2e8709c76ba5a7fafa3fae2360bf9c554e6b113
tonsit <tonsit@gmail.com>
2021-02-26 02:41:11 -0500

    bump up wearntear gui

--- InteractWhileBuilding/InteractWhileBuilding.cs
+++ InteractWhileBuilding/InteractWhileBuilding.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using UnityEngine;
+
 
 namespace InteractWhileBuilding
 {
@@ -16,7 +18,7 @@ namespace InteractWhileBuilding
 
         const string NAME = "InteractWhileBuilding";
 
-        const string VERSION = "0.0.1";
+        const string VERSION = "0.0.3";
 
         private static ManualLogSource logger;
 
@@ -48,6 +50,14 @@ namespace InteractWhileBuilding
             }
 
             return il.AsEnumerable();            
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Hud), "Awake")]
+        private static void BumpUpPieceHealthRoot(Hud __instance)
+        {
+            Vector3 position = __instance.m_pieceHealthRoot.localPosition;
+            __instance.m_pieceHealthRoot.localPosition = new Vector3(position.x, position.y + 20f, position.z);
         }
 
         public static void Log(object data, LogLevel level = LogLevel.Info)
--- InteractWhileBuilding/README.md
+++ InteractWhileBuilding/README.md
@@ -4,6 +4,8 @@ Allows you to interact with objects in the world while in build mode.
 
 Enables the Use interactions (Default is 'E' on keyboard) for objects like doors, fireplaces, and containers while holding items like the hammer and hoe.
 
+Adjusts the position of the WearNTear GUI element up slightly so that it doesn't overlap with the action text.
+
 ## Installation
 
 1. Download and install BepInEx Valheim
@@ -11,3 +13,11 @@ Enables the Use interactions (Default is 'E' on keyboard) for objects like doors
 3. Launching the game will generate a config file at `<GameLocation>\BepInEx\config`
 
 There is no configuration for this plugin.
+
+## Changelog
+- v0.0.3
+	- Bump up WearNTear GUI element
+- v0.0.2
+	- Update readme
+- v0.0.1
+	- initial release
