342a6c1ffa0c97a434b0c78a6f42c8f69e5ea7a9
tonsit <tonsit@gmail.com>
2021-03-01 01:42:36 -0500

    Add configuration for InteractWhileBuilding

--- InteractWhileBuilding/InteractWhileBuilding.cs
+++ InteractWhileBuilding/InteractWhileBuilding.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
+using BepInEx.Configuration;
+
 
 
 namespace InteractWhileBuilding
@@ -18,15 +20,27 @@ namespace InteractWhileBuilding
 
         const string NAME = "InteractWhileBuilding";
 
-        const string VERSION = "0.0.3";
+        const string VERSION = "1.0.0";
 
         private static ManualLogSource logger;
 
+        static ConfigEntry<float> configXPadding;
+        static ConfigEntry<float> configYPadding;
+
         void Awake()
         {
-
             logger = Logger;
             Log("InteractWhileBuilding loaded.");
+
+            string sectionName = "Padding for WearNTear GUI";
+
+            configXPadding = Config.Bind(sectionName, "X-axis", 0f,
+                new ConfigDescription($"Modify the horizontal positioning of the element",
+                    new AcceptableValueRange<float>(-1000, 1000)));
+
+            configYPadding = Config.Bind(sectionName, "Y-axis", 20f,
+                new ConfigDescription($"Modify the vertical positioning of the element",
+                    new AcceptableValueRange<float>(-1000, 1000)));
 
             var harmony = new Harmony(GUID);
             harmony.PatchAll();
@@ -60,7 +74,17 @@ namespace InteractWhileBuilding
         private static void BumpUpPieceHealthRoot(Hud __instance)
         {
             Vector3 position = __instance.m_pieceHealthRoot.localPosition;
-            __instance.m_pieceHealthRoot.localPosition = new Vector3(position.x, position.y + 20f, position.z);
+            __instance.m_pieceHealthRoot.localPosition = new Vector3(position.x + GetConfigXPadding(), position.y + GetConfigYPadding(), position.z);
+        }
+
+        static float GetConfigXPadding()
+        {
+            return configXPadding.Value;
+        }
+
+        static float GetConfigYPadding()
+        {
+            return configYPadding.Value;
         }
 
         public static void Log(object data, LogLevel level = LogLevel.Info)
--- InteractWhileBuilding/README.md
+++ InteractWhileBuilding/README.md
@@ -6,15 +6,34 @@ Enables the Use interactions (Default is 'E' on keyboard) for objects like doors
 
 Adjusts the position of the WearNTear GUI element up slightly so that it doesn't overlap with the action text.
 
+The X and Y axis of the element can be adjusted.
+
 ## Installation
 
 1. Download and install BepInEx Valheim
 2. Download this mod and move the `InteractWhileBuilding.dll` into `<GameLocation>\BepInEx\plugins`
 3. Launching the game will generate a config file at `<GameLocation>\BepInEx\config`
 
-There is no configuration for this plugin.
+## Configuration
+```
+[Padding for WearNTear GUI]
+
+## Modify the horizontal positioning of the element
+# Setting type: Single
+# Default value: 0
+# Acceptable value range: From -1000 to 1000
+X-axis = 0
+
+## Modify the vertical positioning of the element
+# Setting type: Single
+# Default value: 20
+# Acceptable value range: From -1000 to 1000
+Y-axis = 20
+```
 
 ## Changelog
+- v1.0.0
+	- Add configuration to adjust GUI X,Y positioning
 - v0.0.3
 	- Bump up WearNTear GUI element
 - v0.0.2
